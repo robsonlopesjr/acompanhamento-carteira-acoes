c7fffeff9575851b7d1e1d5d8e566f9539b65163
Robson José Lopes G. Júnior <robsonlopes.jr@gmail.com>
2025-01-11 15:23:44 -0300

    exibindo mais de uma ação no gráfico

--- src/main.py
+++ src/main.py
@@ -3,20 +3,36 @@ import yfinance as yf
 
 
 @st.cache_data
-def carregar_dados(empresa):
-    dados_acao = yf.Ticker(ticker=empresa)
-    cotacoes_acao = dados_acao.history(
-        period="1d", start="2010-01-01", end="2024-12-01"
-    )["Close"]
+def carregar_dados(empresas):
+    texto_tickers = " ".join(empresas)
+    dados_acao = yf.download(tickers=texto_tickers, period="1d", start="2010-01-01", end="2024-12-01")
+    cotacoes_acao = dados_acao["Close"]
     return cotacoes_acao
 
 
-dados = carregar_dados(empresa="ITUB4.SA")
+acoes = [
+    "ITUB4.SA",
+    "PETR4.SA",
+    "MGLU3.SA",
+    "VALE3.SA",
+    "ABEV3.SA",
+    "GGBR4.SA",
+]
+dados = carregar_dados(empresas=acoes)
+
+
+lista_acoes = st.multiselect(label="Ações", options=dados.columns, placeholder="Escolha as ações para visualizar...")
+if lista_acoes:
+    dados = dados[lista_acoes]
+
+    if len(lista_acoes) == 1:
+        acao_unica = lista_acoes[0]
+        dados = dados.rename(columns={acao_unica: "Close"})
 
 st.write(
     """
 # App Preço de Ações
-O gráfico abaixo representa a evolução do preço das ações do Itaú (ITUB4) ao longo dos anos.
+O gráfico abaixo representa a evolução do preço das ações ao longo dos anos.
 """
 )
 
